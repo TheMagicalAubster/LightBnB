3c1203153e096e820970f966992350ba2b3f5ee8
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-06-25 22:06:31 +0000

    search options

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,7 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 const pg = require('pg');
+const { query } = require('express');
 
 
 /// Users
@@ -85,13 +86,53 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-  .then(res => res.rows);
+    const queryParams = [];
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
   
-}
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+    if(options.minimum_price_per_night) {
+      queryParams.push(`${options.minimum_price_per_night}`);
+      queryString +=`AND cost_per_night > $${queryParams.length} `;
+    }
+    if(options.maximum_price_per_night) {
+      queryParams.push(`${options.maximum_price_per_night}`);
+      queryString +=`AND cost_per_night < $${queryParams.length} `;
+    }
+
+    if(options.minimum_rating) {
+      queryParams.push(`${options.minimum_rating}`);
+      queryString +=`AND rating >= $${queryParams.length} `;
+    }
+
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+    console.log(queryString, queryParams);
+  
+    return pool.query(queryString, queryParams)
+    .then(res => res.rows);
+  }
+  
+  
+  
+  // return pool.query(`
+  // SELECT * FROM properties
+  // LIMIT $1
+  // `, [limit])
+  // .then(res => res.rows);
+  
+// }
 exports.getAllProperties = getAllProperties; 
 
 
